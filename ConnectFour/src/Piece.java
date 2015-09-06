ffa1f4380f1911100c669f7fb14f480ae4f1b31d
michaelrinos <mrinos98@gmail.com>
2015-09-05 22:23:35 -0400

    Initial commit

---
+++ ConnectFour/src/Piece.java
@@ -0,0 +1,14 @@
+/**
+ * Created by michael on 5/9/2015.
+ */
+public class Piece {
+    private int owner;
+
+    public Piece(int owner){
+        this.owner = owner;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+}
